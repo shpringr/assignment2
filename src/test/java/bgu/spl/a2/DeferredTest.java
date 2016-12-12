083de2e08d2a8200d29eb9a7769cfe1d78c38e98
orel hazan <Shpringr@post.bgu.ac.il>
2016-12-12 13:16:47 +0200

    scs

--- src/test/java/bgu/spl/a2/DeferredTest.java
+++ src/test/java/bgu/spl/a2/DeferredTest.java
@@ -1,92 +1,30 @@
-package java.bgu.spl.a2;
+package bgu.spl.a2;
 
-import java.util.function.Function;
-import java.util.function.Predicate;
-
-import bgu.spl.a2.Deferred;
-import org.jetbrains.annotations.Contract;
-import org.junit.Assert;
 import org.junit.Test;
 
-
-/**
- * this class represents a deferred result i.e., an object that eventually will
- * be resolved to hold a result of some operation, the class allows for getting
- * the result once it is available and registering a callback that will be
- * called once the result is available.
- *
- * Note for implementors: you may add methods and synchronize any of the
- * existing methods in this class *BUT* you must be able to explain why the
- * synchronization is needed. In addition, the methods you add can only be
- * private, protected or package protected - in other words, no new public
- * methods
- *
- * @param <T> the result type
- */
 public class DeferredTest<T> extends Deferred {
 
-
-    public DeferredTest(){
-
-    }
-
-    /**
-     *
-     * @return the resolved value if such exists (i.e., if this object has been
-     * {@link #resolve(java.lang.Object)}ed yet
-     * @throws IllegalStateException in the case where this method is called and
-     * this object is not yet resolved
-     */
-
-    @Test public void testGet(){
+    @Test
+    public void testGet(){
         //TODO: replace method body with real implementation
         throw new UnsupportedOperationException("Not Implemented Yet.");
     }
 
-
-    /**
-     *
-     * @return true if this object has been resolved - i.e., if the method
-     * {@link #resolve(java.lang.Object)} has been called on this object before.
-     */
-    @Test public void testIsResolved() {
+    @Test
+    public void testIsResolved() {
         //TODO: replace method body with real implementation
         throw new UnsupportedOperationException("Not Implemented Yet.");
     }
 
-    /**
-     * resolve this deferred object - from now on, any call to the method
-     * {@link #get()} should return the given value
-     *
-     * Any callbacks that were registered to be notified when this object is
-     * resolved via the {@link #whenResolved(java.lang.Runnable)} method should
-     * be executed before this method returns
-     *
-     * @param value - the value to resolve this deferred object with
-     * @throws IllegalStateException in the case where this object is already
-     * resolved
-     */
-    @Test public void testResolve() {
+    @Test
+    public void testResolve() {
         //TODO: replace method body with real implementation
         throw new UnsupportedOperationException("Not Implemented Yet.");
     }
 
-    /**
-     * add a callback to be called when this object is resolved. if while
-     * calling this method the object is already resolved - the callback should
-     * be called immediately
-     *
-     * Note that in any case, the given callback should never get called more
-     * than once, in addition, in order to avoid memory leaks - once the
-     * callback got called, this object should not hold its reference any
-     * longer.
-     *
-     * @param callback the callback to be called when the deferred object is
-     * resolved
-     */
-    @Test public void testWhenResolved() {
+    @Test
+    public void testWhenResolved() {
         //TODO: replace method body with real implementation
         throw new UnsupportedOperationException("Not Implemented Yet.");
     }
-
 }
