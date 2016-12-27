5350f40251c2431c05eb974316fe8b9d3c7d6ef3
Orel Hazan <orelhaz@post.bgu.ac.il>
2016-12-27 15:16:59 +0200

    hiii

--- src/main/java/bgu/spl/a2/sim/Simulator.java
+++ src/main/java/bgu/spl/a2/sim/Simulator.java
@@ -27,11 +27,11 @@ import java.util.concurrent.CountDownLatch;
 /**
  * A class describing the simulator for part 2 of the assignment
  */
-public class Simulator  {
+public class Simulator {
 
     private static WorkStealingThreadPool workStealingThreadPool;
     private static List<Wave> waves = new ArrayList<>();
-    private static Warehouse warehouse= new Warehouse();
+    private static Warehouse warehouse = new Warehouse();
 
     /**
      * Begin the simulation
@@ -39,50 +39,69 @@ public class Simulator  {
      */
     public static ConcurrentLinkedQueue<Product> start() {
 
-        ConcurrentLinkedQueue<Product> products = new ConcurrentLinkedQueue<>();
+        ConcurrentLinkedQueue<Product> manufacturedProducts = new ConcurrentLinkedQueue<>();
 
         workStealingThreadPool.start();
 
-        for (Wave currWave: waves) {
+        for (Wave currWave : waves) {
 
-            int numberOfTasks = 0;
+            CountDownLatch l = new CountDownLatch(getNumberOfProductsInWave(currWave));
+            List<Product> productsInWave = getWaveProducts(currWave);
 
-            for (Order curr : currWave.getOrders())
+            for (Product currProduct : productsInWave) 
             {
-                numberOfTasks+= curr.getQty();
+                ManufacturingTask currTask = new ManufacturingTask(currProduct, warehouse);
+                
+                Simulator.workStealingThreadPool.submit(currTask);
+
+                currTask.getResult().whenResolved(l::countDown);
             }
-
-            CountDownLatch l = new CountDownLatch(numberOfTasks);
-
-            for (Order order : currWave.getOrders()) {
-
-                for (int i = 0; i < order.getQty(); i++)
-                {
-                    Product currProduct = new Product(order.getStartId() + i , order.getProduct());
-                    ManufacturingTask currTask = new ManufacturingTask(currProduct,warehouse);
-                    Simulator.workStealingThreadPool.submit(currTask);
-
-                    currTask.getResult().whenResolved(() -> {
-                        l.countDown();
-                        products.add(currTask.getResult().get());
-                    });
-
-                }
-            }
-
-            try {
+            try 
+            {
                 l.await();
-            } catch (InterruptedException e) {
+                
+                for (Product product : productsInWave)
+                    manufacturedProducts.add(product);
+            }
+            catch (InterruptedException e) 
+            {
                 e.printStackTrace();
             }
         }
-        try {
-            workStealingThreadPool.shutdown(); //stopping all the threads
-        } catch (InterruptedException e) {
+        try
+        {
+            workStealingThreadPool.shutdown();
+        }
+        catch (InterruptedException e)
+        {
             e.printStackTrace();
         }
 
-        return products;
+        return manufacturedProducts;
+    }
+
+    private static List<Product> getWaveProducts(Wave wave)
+    {
+        List<Product> productList = new ArrayList<>();
+
+        for (Order order : wave.getOrders())
+        {
+            for (int i = 0; i < order.getQty(); i++) {
+                productList.add(new Product(order.getStartId() + i, order.getProduct()));
+            }
+        }
+        
+        return productList;
+    }
+
+    private static int getNumberOfProductsInWave(Wave wave)
+    {
+        int numberOfTasks = 0;
+
+        for (Order curr : wave.getOrders()) {
+            numberOfTasks += curr.getQty();
+        }
+        return numberOfTasks;
     }
 
     /**
@@ -94,56 +113,52 @@ public class Simulator  {
         workStealingThreadPool = myWorkStealingThreadPool;
     }
 
-
-    private static void addTool(String toolType, int qty){
-        if (toolType.equals("gs-driver")){
+    private static void addTool(String toolType, int qty) {
+        if (toolType.equals("gs-driver")) {
             GcdScrewDriver tool = new GcdScrewDriver();
             warehouse.addTool(tool, qty);
-        }
-        else if (toolType.equals("np-hammer")){
+        } else if (toolType.equals("np-hammer")) {
             NextPrimeHammer tool = new NextPrimeHammer();
             warehouse.addTool(tool, qty);
-        }
-        else if (toolType.equals("rs-pliers")){
+        } else if (toolType.equals("rs-pliers")) {
             RandomSumPliers tool = new RandomSumPliers();
             warehouse.addTool(tool, qty);
         }
     }
 
     public static void main(String[] args) {
-        String jasonFileLocation = args[0];
-        Gson gson = new Gson();
+        try
+        {
+            String jasonFileLocation = args[0];
+            Gson gson = new Gson();
 
-        try {
             BufferedReader br = new BufferedReader(new FileReader(jasonFileLocation));
 
             //convert the json string back to object
             ParseData obj = gson.fromJson(br, ParseData.class);
             WorkStealingThreadPool workStealingThreadPoolTmp = new WorkStealingThreadPool(obj.getThreads());
-            Simulator.attachWorkStealingThreadPool(workStealingThreadPoolTmp);
 
             //add tool to Warehouse
-            for (int i = 0; i < obj.getTools().size() ; i++) {
-                addTool(obj.getTools().get(i).getTool(),obj.getTools().get(i).getQty() );
+            for (int i = 0; i < obj.getTools().size(); i++) {
+                addTool(obj.getTools().get(i).getTool(), obj.getTools().get(i).getQty());
             }
             ManufactoringPlan plan;
             for (int i = 0; i < obj.getPlans().size(); i++) {
                 String product = obj.getPlans().get(i).getProduct();
-                String[] parts= obj.getPlans().get(i).getParts();
+                String[] parts = obj.getPlans().get(i).getParts();
                 String[] tools = obj.getPlans().get(i).getTools();
-                plan = new ManufactoringPlan(product,parts ,tools);
+                plan = new ManufactoringPlan(product, parts, tools);
                 warehouse.addPlan(plan);
             }
 
             Wave wave;
             int size = obj.getWaves().size();
-            for (int i = 0; i < obj.getWaves().size(); i++) {
-               wave = new Wave(obj.getWaves().get(i));
-               waves.add(wave);
+            for (int i = 0; i < size; i++) {
+                wave = new Wave(obj.getWaves().get(i));
+                waves.add(wave);
             }
 
-            //System.out.println(obj.toString());
-
+            Simulator.attachWorkStealingThreadPool(workStealingThreadPoolTmp);
 
             ConcurrentLinkedQueue<Product> simulationResult;
             simulationResult = Simulator.start();
@@ -155,8 +170,6 @@ public class Simulator  {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        //return 0;
     }
 
 }
